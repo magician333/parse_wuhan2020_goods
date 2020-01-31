2117485d023cec56a857462ca31c6aabd577476a
Purplefire <magician33333@gmail.com>
2020-01-31 20:07:17 +0800

    Add files via upload

--- parse_goods.py
+++ parse_goods.py
@@ -8,14 +8,14 @@ temp_dict = dict()
 info = """    武汉加油！！！
 -物资解析器-
 -purplefire 紫火-
----输入格式分隔为;；，、:： 格式为：数字*物品---
+---输入格式分隔为; 格式为：数字*物品---
 ---模式切换请设置---"""
 
 
 def main(kinds, text):
     with open(time.strftime('%H-%M-%S', time.localtime(time.time()))+".json", "w", encoding="utf-8") as output_file:
 
-        for i in items.split(";，、；：:"):
+        for i in items.split(";"):
             # 格式 物资 数量 如n95口罩 3000
             if kinds == 1:
                 goods, num = i.split(" ")
@@ -39,7 +39,7 @@ if __name__ == '__main__':
             print(info)
             items = input("输入需要解析的数据：")
             # 此处设置模式，默认模式2
-            main(2, items)
+            main(1, items)
             print("\n--------解析完成--------\n\n\n")
         except KeyboardInterrupt:
             print("\n--------感谢使用--------\n")
